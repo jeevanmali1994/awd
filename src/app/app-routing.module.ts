e16895df5e8209d7e54dddb84cbcd441e4e63416
Jeevan Mali <jeevanmali1994@gmail.com>
2020-05-28 21:02:50 +0530

    first attempt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { EducationComponent } from './education/education.component';
+import { AchievementComponent } from './achievement/achievement.component';
+import { PersonaldetailsComponent } from './personaldetails/personaldetails.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+{ path: 'first' , component: FirstComponent },
+{ path: 'personal' ,component: PersonaldetailsComponent },
+{ path: 'achievement', component: AchievementComponent },
+{ path: 'education', component: EducationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { EducationComponent } from './education/education.component';
+import { AchievementComponent } from './achievement/achievement.component';
+import { PersonaldetailsComponent } from './personaldetails/personaldetails.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    EducationComponent,
+    AchievementComponent,
+    PersonaldetailsComponent
   ],
   imports: [
     BrowserModule,
